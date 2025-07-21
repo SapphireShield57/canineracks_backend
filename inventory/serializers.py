deab7124f12f454e8c080eb8e3072a720203cd62
SapphireShield57 <lgsingayanshs@gmail.com>
2025-07-22 00:37:31 +0800

    Update serializers.py

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -2,17 +2,13 @@ from rest_framework import serializers
 from .models import Product, StockHistory
 
 class ProductSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
+    image = serializers.ImageField(required=False, allow_null=True)
 
     class Meta:
         model = Product
         fields = '__all__'
-        read_only_fields = ['created_at', 'updated_at']  # prevent overriding timestamps
+        read_only_fields = ['created_at', 'updated_at']
 
-    def get_image(self, obj):
-        if obj.image:
-            return obj.image.url  # Returns Cloudinary URL or default media URL
-        return None
 
 
 class StockHistorySerializer(serializers.ModelSerializer):
