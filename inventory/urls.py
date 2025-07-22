9a86d0db7e4cbb61e7ff14256009d0da3acc0429
SapphireShield57 <lgsingayanshs@gmail.com>
2025-07-22 23:26:18 +0800

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, RecommendationView
+from .views import ProductViewSet, RecommendationView, OrderCreateView
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
@@ -8,4 +8,5 @@ router.register('products', ProductViewSet, basename='product')
 urlpatterns = [
     path('', include(router.urls)),
     path('recommendations/', RecommendationView.as_view(), name='product-recommendations'),
+    path('order/create/', OrderCreateView.as_view(), name='order-create'),
 ]
